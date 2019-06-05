31d3f6f47270bf4f23a7c104372bd05617258687
Alicia Raulet <webdesigner.form@gmail.com>
2019-06-05 16:57:37 +0200

    #1 Issue taken care of

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,28 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $anonymous = new User();
+        $anonymous->setUsername('Anonymous');
+        $anonymous->setEmail('anonymous@todo.com');
+        $anonymous->setPassword($this->encoder->encodePassword($anonymous, 'anonymous'));
+        $anonymous->setRole('ROLE_USER');
+        $manager->persist($anonymous);
         $manager->flush();
     }
 }
